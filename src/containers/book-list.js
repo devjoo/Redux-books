66a9cbf4850ddd7b344802bc6e503d75cb7c2f74
sona <chookin77@naver.com>
2017-11-20 02:39:54 +0900

    action ->  import { bindActionCreators } from 'redux';

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import selectBook from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
     renderList(){
@@ -28,7 +30,16 @@ function mapStateToProps(state) {
     
 }
 
-export default connect(mapStateToProps)(BookList);
+
+// 이 함수로 반환받은 것이 북 리스트 컨테이너의 props로 연결될 것 이다
+function mapDispatchToProps(dispatch) {
+    // selectBook이 호출될때 마다 , 결과는 리듀서로 전달되어야만 한다
+    return bindActionCreators({ selectBook : selectBook }, dispatch);
+}
+
+// 컴포넌트에서 컨테이너로 북리스트를 바꿔야 하는데 이는 새로운 dispatch 메소드인
+// selectBook을 알아야 하고 이는 prop로 이용이 가능하다
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 
 
 
