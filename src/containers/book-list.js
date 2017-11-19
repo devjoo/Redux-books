0ebe7ef36f879d2f10e7f3a5be20141b941ce64a
sona <chookin77@naver.com>
2017-11-19 23:57:43 +0900

    import { connect } from 'react-redux'; 사용

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component{
+class BookList extends Component{
     renderList(){
         return(
-            this.props.books.map((book, index)=>{
+            this.props.books.map((book, index) => {
                 return <li key={index} className="list-group-item">{book.title}</li>
             })
         )
     }
 
     render(){
+        console.log(this.props.books); // -> 리턴
         return(
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -18,6 +20,18 @@ export default class BookList extends Component{
     }
 }
 
+function mapStateToProps(state) {
+    // 무엇이라도 props로 리턴된다
+    return{
+        books: state.books
+    };
+    
+}
+
+export default connect(mapStateToProps)(BookList);
+
+
+
 /*
 * 컨테이너는 리덕스에 의해 만들어지는 스테이트를 직접 접근하 컴포넌트
-* */
+ */
